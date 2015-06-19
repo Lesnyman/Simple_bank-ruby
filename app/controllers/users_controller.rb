59b3cc52cc9979e73d3ff68d8e48f985fb5882de
Tomasz Leśniewski <t.lesniewskii@gmail.com>
2015-06-19 21:22:17 +0200

    minor fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   #before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :logged_in_user, only: [:edit, :update, :show, :index, :destroy]
-  before_action :correct_user,   only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update, :show, :index, :destroy, :profile]
+  before_action :correct_user,   only: [:edit, :update, :profile]
   before_action :admin_user,     only: :destroy
 
   def index
@@ -9,6 +9,10 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+  
+  def profile
     @user = User.find(params[:id])
   end
 
